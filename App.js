88990f87da97eb2ff573469c255a4d0733c219ff
Woojin <sam4woojin@naver.com>
2021-07-06 10:46:23 +0900

    Get Position

--- App.js
+++ App.js
@@ -1,12 +1,10 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
 
-export default function App() {
+export default function Loading() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Text style={styles.text}>Getting Weather</Text>
     </View>
   );
 }
@@ -14,8 +12,14 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    justifyContent: "flex-end",
+    paddingHorizontal: 30,
+    paddingVertical: 100,
+    backgroundColor: "#FDF6AA",
+  },
+
+  text: {
+    color: "#2c2c2c",
+    fontSize: 20,
   },
 });
